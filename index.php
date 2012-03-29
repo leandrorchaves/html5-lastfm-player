cb10fdc592d5edefb3d119ef58665faf38149bbb
Tobias Brennecke <t.burny@gmail.com>
2012-03-30 00:17:49 +0200

    update to jplayer 2.1.0

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 #If not logged in, redirect to lastfm login page.
 session_start();
 if (!isset($_SESSION['sessionKey'])){
-	#Not sure if this is a strong enough constraint, but good enough for now.
-	header("Location: http://www.last.fm/api/auth/?api_key=a93b385d81161eb5ed308da9d734a7a5&cb=http://" . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/callback.php");
+    #Not sure if this is a strong enough constraint, but good enough for now.
+    header("Location: http://www.last.fm/api/auth/?api_key=a93b385d81161eb5ed308da9d734a7a5&cb=http://" . $_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/callback.php");
 
 }else{
-	header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/radio.php");
+    header("Location: http://".$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF'])."/radio.php");
 }
 ?>
